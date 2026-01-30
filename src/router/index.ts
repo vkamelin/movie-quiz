48f3e73a59b605684a7abc4584e91a95916a5845
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-01-30 22:33:53 +0500

    feat: Добавлены маршруты для игры и таблицы лидеров
    Добавлены маршруты
    '/': компонент Game
    '/leaderboard': компонент Leaderboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import type {RouteRecordRaw} from 'vue-router'
+import Game from "../pages/Game.vue";
+import Leaderboard from "../pages/Leaderboard.vue";
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    redirect: '/game'
-  },
+    {
+        path: '/',
+        name: 'home',
+        component: Game
+    },
+    {
+        path: '/leaderboard',
+        name: 'leaderboard',
+        component: Leaderboard
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
