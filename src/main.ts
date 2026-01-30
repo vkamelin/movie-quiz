6dc6329ff247888f90f4feee2d7c4cd40b7904a1
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-01-30 22:12:32 +0500

    feat: Добавил Router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router from './router'
 import './style.css'
 import App from './App.vue'
 
@@ -13,6 +14,9 @@ app.config.globalProperties.$http = axios
 // Pinia configuration
 const pinia = createPinia()
 app.use(pinia)
+
+// Router configuration
+app.use(router)
 
 // Mount the app
 app.mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/game'
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
