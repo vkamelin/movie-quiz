35f8aa0c31d81794fb82168760977cf3870392a8
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-01-31 21:36:04 +0500

    feat: добавить компонент UIButton
    Создает переиспользуемый компонент кнопки с базовым стилем Tailwind CSS.
    Кнопка принимает слот для контента и эмитит событие 'click'.

---
+++ src/components/UI/UIButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="$emit('click')">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+@reference "tailwindcss";
+button {
+  @apply bg-white/25 text-white font-bold py-2 px-4 rounded-xl border border-white/25 hover:bg-white/50;
+}
+</style>
