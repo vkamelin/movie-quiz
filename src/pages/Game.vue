003e5b725a93f4f382bed3e9077cfa540702cad2
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-01-31 20:53:13 +0500

    feat: добавить базовый интерфейс игры
    Добавлен компонент Game.vue с
    начальным отображением заголовка,
    вопроса и вариантов ответов.
    Стилизовано с использованием
    Tailwind CSS для центрирования
    и оформления элементов.

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Игра</h1>
+  <div class="game">
+    <h3>Отгадай фильм:</h3>
+    <div class="question">🤖👦🕶️💥🚗👁️‍🗨️</div>
+    <div class="answers">
+      <div class="answer">Терминатор 2</div>
+      <div class="answer">Робокоп</div>
+      <div class="answer">Я - робот</div>
+      <div class="answer">Звездные войны</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +16,32 @@
 </script>
 
 <style scoped>
+@reference "tailwindcss";
 
+.game {
+  @apply flex flex-col items-center justify-center gap-4 h-full;
+}
+
+.game h3 {
+  @apply text-2xl font-bold mb-4;
+}
+
+.game .question {
+  @apply text-3xl font-bold mb-8 bg-white/90 p-4 rounded-lg;
+}
+
+/* 2 per row */
+.game .answers {
+  /*@apply grid grid-cols-2 gap-4;*/
+  @apply flex flex-col w-full;
+}
+
+.game .answer {
+  @apply border-2 border-white/25 p-2 rounded-lg text-center w-full mb-4;
+}
+
+.game .answer:hover,
+.game .answer:focus {
+  @apply bg-white/25;
+}
 </style>
